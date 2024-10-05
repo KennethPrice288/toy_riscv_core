0236bc72508a40addf7015a6e82c71742701b69d
Kenneth Price <krprice@ucsc.edu>
2024-10-05 12:12:22 -0700

    accidentally included sim_build remnants

--- src/dm_sim_build/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("/Users/kennethprice/Documents/Personal_Projects/toy_processor/src/dm_sim_build/data_memory.fst");
-    $dumpvars(0, data_memory);
-end
-endmodule
